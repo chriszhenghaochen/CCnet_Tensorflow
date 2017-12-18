285a36745383744d996093f9baa83ec6ccc206ad
chriszhenghaochen <chris.zhenghaochen@gmail.com>
2017-12-18 14:13:24 +0800

    tmp file

--- lib/model/nms_wrapper.py
+++ lib/model/nms_wrapper.py
@@ -17,9 +17,7 @@ def nms(dets, thresh, force_cpu=False):
 
   if dets.shape[0] == 0:
     return []
-  # if cfg.USE_GPU_NMS and not force_cpu:
-  #   return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
-  # else:
-  #   return cpu_nms(dets, thresh)
-
-  return cpu_nms(dets, thresh)
+  if cfg.USE_GPU_NMS and not force_cpu:
+    return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
+  else:
+    return cpu_nms(dets, thresh)
